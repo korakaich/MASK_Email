1b9be6c05957ddcd37ff08a172a8b014c959634d
korakaich <korak103@gmail.com>
2012-11-20 17:49:54 -0500

    first commit

--- OutsourcedEmail/src/org/mask/outsourcedemail/Authenticator.java
+++ OutsourcedEmail/src/org/mask/outsourcedemail/Authenticator.java
@@ -2,7 +2,10 @@ package org.mask.outsourcedemail;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.*;
 
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,10 +16,12 @@ import javax.servlet.http.HttpSession;
 /**
  * Servlet implementation class Authenticator
  */
-@WebServlet("/AuthenticatorPath")
+//@WebServlet("/AuthenticatorPath")
 public class Authenticator extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+	private Connection con;  
+	private Statement st;  
+	private ResultSet rs;       
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -24,7 +29,34 @@ public class Authenticator extends HttpServlet {
         super();
         // TODO Auto-generated constructor stub
     }
-
+    
+    /*public void init(ServletConfig config) throws ServletException {
+    	String url = "jdbc:mysql://localhost:3306/emailServer";
+ 	    String dbName = "root";  
+ 	    String dbPassword = "";
+ 	    
+      	String driver = config.getInitParameter("Driver");  
+        String url = config.getInitParameter("URL");  
+        String userName = config.getInitParameter("UserName");  
+        String password = config.getInitParameter("Password");
+    	  
+        String url = "jdbc:mysql://localhost:3306/emailServer";  
+        String userName = "root"; 
+        String password = "";
+    	
+        
+        try {  
+        	Class.forName("com.mysql.jdbc.Driver");
+        	con = DriverManager.getConnection(url, userName, password);
+            st = con.createStatement();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}        
+        ServletContext context = getServletContext();
+        context.setAttribute("connection", con); // Save DB Connection as an attribute                       
+     }  */
+  
+    
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
@@ -34,8 +66,11 @@ public class Authenticator extends HttpServlet {
 		PrintWriter writer= response.getWriter();
 		writer.println("<h4>hellooo..."+userName+"(gigiti gigiti) eh eh</h4>");
 	}
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		HttpSession session = request.getSession(true);
+		ServletContext context = session.getServletContext();
+		
 	    if(session == null || session.isNew()){
 	       //valid session doesn't exist
 	       //do something like send the user to a login screen
@@ -43,17 +78,63 @@ public class Authenticator extends HttpServlet {
 	    }
 	    else{
 	    	String userName=(String)session.getAttribute("name");
-	    	System.out.println(session.getId());
+	    	
+	    	  
+	    	String url = "jdbc:mysql://localhost:3306/emailServer";
+	 	    String dbName = "root";  
+	 	    String dbPassword = "";
+	 	    
+	 	    //System.out.println("URL::"+url);
+	 	    //System.out.println("username::"+userName);
+	 	    String rpasswd="";
+	 	    try {  
+	 	    	Class.forName("com.mysql.jdbc.Driver");
+	 	    	con = DriverManager.getConnection(url, dbName, dbPassword);
+	 	        st = con.createStatement();
+	 	        st = con.createStatement();
+	 		    rs = st.executeQuery("SELECT * FROM user where uname='"+userName+"'");
+	 		    while(rs.next()){
+	 		    	rpasswd=rs.getString(3);
+	 		    }
+	 		   System.out.println("DB is"+rpasswd);
+	 	    } catch (Exception e) {
+	 			e.printStackTrace();
+	 		}	    	    
+	 	    finally{	 	    	
+	 	    	try {
+	 	    		if(rs != null) {
+	 	    			rs.close();
+	 	    			rs = null;
+	 	    		}
+	 	    		if(st != null) {
+	 	    			st.close();
+	 	    			st = null;
+	 	    		}
+	 	    		if(con != null) {
+	 	    			con.close();
+	 	    			con = null;
+	 	    		}
+	 	    	} catch (SQLException e) {}
+	 	    }
+	 	    
+	        
 	    	if(userName == null){
 	    		//no username in session..this should never happen ... validation check by javascript 	    
 	    		//user probably hasn't logged in properly
 	    		System.out.println("WHY U no give username");
 	    	}	    	
 	    	PrintWriter writer= response.getWriter();
-	    	if( userName.equals("korak") ){
-	    		String passwd=request.getParameter("password");
-	    		if(passwd.equals("letmein")){	    		
-	    			writer.println("<h4>hellooo..."+userName+"You are in!</h4>");
+	    	
+	    	if( true) {
+	    		String passwd=request.getParameter("hashedvalue");
+	    		String salt=request.getParameter("Salt");
+	    		System.out.println(salt);
+	    		System.out.println("Entered pwd::"+passwd);
+	    		if(passwd.equals(rpasswd)){	    			    			  
+	    			response.sendRedirect("https://localhost:8443/OutsourcedEmail/home.jsp");	    			
+	                /*if(context.getAttribute("connection")==null){
+	                	System.out.println("Null hai");
+	                }*/	                	
 	    			session.setAttribute("logged", true);
 	    			session.setMaxInactiveInterval(1);
 	    		}
@@ -61,9 +142,7 @@ public class Authenticator extends HttpServlet {
 	    			writer.println("<h4>hellooo..."+userName+"wtf!</h4>");
 	    		}
 	    	}
-	    	else{
-	    		writer.println("<h4>wtf!</h4>");
-	    	}
+	    	
 	    }
 	}
 }
